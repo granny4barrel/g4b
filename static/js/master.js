53f41fc1c207a900af9c00cb279856150a408140
jvd33 <jvd5839@g.rit.edu>
2017-01-18 02:27:57 -0500

    V1

--- static/js/master.js
+++ static/js/master.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
 
     $('.item').hover(function() {
-        $(this).css('background-color','rgba(154, 91, 51, 1)');
-
+        $(this).css('background-color','rgba(169, 97, 21, .8)').css('color', 'rgba(30, 32, 34, 1)');
     },
     function() {
-        $(this).css('background-color','rgba(169, 97, 21, .8)');
+        $(this).css('background-color','rgba(154, 91, 51, 1)').css('color', 'rgba(225, 223, 221, 1)');
     });
 
     $('.ui.modal').click(function(){
@@ -34,7 +33,7 @@ $(document).ready(function() {
         ready:
             function() {
                 $(this).jPlayer('setMedia', {
-                    title: 'Guns 30 Second Clip',
+                    title: 'She Likes Guns - 30 seconds',
                     mp3: 'assets/audio/Guns 30 Second Clip.01_01.mp3'
                 });
             },
@@ -52,8 +51,3 @@ $(document).ready(function() {
 
     });
 });
-
-function assignModalBehavior(el) {
-
-
-}
