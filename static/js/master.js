efac9049b65374ff99ea1efc58f4c1c5b30a80b3
Steve Vicario <vicario@webprosny.com>
2017-10-09 17:16:32 -0400

    Updates for Press Release
    I have updated the video, all the photos, navigation, press release, bio, and new songs.

--- static/js/master.js
+++ static/js/master.js
@@ -33,8 +33,8 @@ $(document).ready(function() {
         ready:
             function() {
                 $(this).jPlayer('setMedia', {
-                    title: 'She Likes Guns - 30 seconds',
-                    mp3: 'assets/audio/Guns 30 Second Clip.01_01.mp3'
+                    title: 'Freak Flag “censored”',
+                    mp3: 'assets/audio/freak-flag-clean-version.mp3'
                 });
             },
         cssSelectorAncestor:'#jp_container_1',
@@ -55,8 +55,8 @@ $(document).ready(function() {
         ready:
             function() {
                 $(this).jPlayer('setMedia', {
-                    title: 'Nitro Sexy - 30 seconds',
-                    mp3: 'assets/audio/Nitro Sexy EPK _30 Snip Mix_01.mp3'
+                    title: 'Freak Flag “censored” (no fiddle)',
+                    mp3: 'assets/audio/freak-flag-alt-clean-version-no-fiddle.mp3'
                 });
             },
         cssSelectorAncestor:'#jp_container_2',
@@ -73,25 +73,4 @@ $(document).ready(function() {
 
     });
 
-    $('#jquery_jplayer3').jPlayer({
-        ready:
-            function() {
-                $(this).jPlayer('setMedia', {
-                    title: 'Freak Flag - 30 seconds',
-                    mp3: 'assets/audio/Freak Flag EPK 30 Snip Mix _01.mp3'
-                });
-            },
-        cssSelectorAncestor:'#jp_container_3',
-
-        swfPath:'static/jquery.jplayer.swf',
-        supplied:'mp3',
-        wmode:'window',
-        useStateClassSkin:true,
-        autoBlur: true,
-        smoothPlayBar: true,
-        keyEnable: true,
-        remainingDuration: true,
-        toggleDuration:true,
-
-    });
 });
